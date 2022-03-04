06030314ecb8e05b1ba52d08ce123eaba77bbe0a
Abayomi Aishida <yomicorperate@gmail.com>
2022-03-04 12:13:52 +0100

    Real time validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,20 @@ import Card from "./shared/Card";
 
 export default function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be at least 10 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
     setText(e.target.value);
   };
 
@@ -22,8 +34,11 @@ export default function FeedbackForm() {
               type="text"
               placeholder="Write a review"
             />
-            <Button type="submit">Send</Button>
+            <Button type="submit" isDisabled={btnDisabled}>
+              Send
+            </Button>
           </div>
+          {message && <div className="message">{message}</div>}
         </h2>
       </form>
     </Card>
