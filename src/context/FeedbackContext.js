acd5bc716c1b7b1de3c0a55672951692052e7fef
Abayomi Aishida <yomicorperate@gmail.com>
2022-03-10 11:37:38 +0100

    Persisting new feedback to backend and proxy setup

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from "uuid";
 import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
@@ -13,9 +12,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const reponse = await fetch(
-      "http://localhost:5000/feedback?_sort=id&_order=desc"
-    );
+    const reponse = await fetch("/feedback?_sort=id&_order=desc");
     const data = await reponse.json();
 
     setFeedback(data);
@@ -43,9 +40,17 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Add Feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+    setFeedback([data, ...feedback]);
   };
 
   const editFeedback = (item) => {
